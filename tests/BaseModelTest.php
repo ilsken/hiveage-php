e283608ec716945a68ebf7d3f25e67b490337b1c
Lance Pioch <lance@bouncefirst.com>
2015-02-11 17:33:09 -0500

    Add a couple tests for the base model.

---
+++ tests/BaseModelTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Bouncefirst\Hiveage\Api\Requestor;
+
+class BaseModelTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructionWithArray()
+    {
+        $base = $this->getMock('Bouncefirst\Hiveage\Models\Base', ['getAttributes'], [['test' => 'hello']]);
+        $base->expects($this->any())->method('getAttributes')->willReturnArgument(0);
+        $this->assertEquals('hello', $base->test);
+    }
+
+    public function testConstructionWithNonArray()
+    {
+        $base = $this->getMock('Bouncefirst\Hiveage\Models\Base', ['getAttributes'], ['test']);
+        $base->expects($this->any())->method('getAttributes')->willReturnArgument(0);
+        $this->assertNull($base->test);
+    }
+
+    public function testSetRequestor()
+    {
+        $requestor = $this->getMock('Bouncefirst\Hiveage\Api\Requestor', [], ['test']);
+        $base = $this->getMock('Bouncefirst\Hiveage\Models\Base');
+        $base->setRequestor($requestor);
+    }
+}
