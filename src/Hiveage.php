482927cbc5fff8043d245625e229a11d7b961e30
Lance Pioch <lance@bouncefirst.com>
2015-02-14 17:39:36 -0500

    Use public function instead.

--- src/Hiveage.php
+++ src/Hiveage.php
@@ -27,66 +27,71 @@ class Hiveage
         $this->requestor = $requestor;
 	}
 
+    public function getRequestor()
+    {
+        return $this->requestor;
+    }
+
     public function getConnections()
     {
         $model = new Connection;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getEstimates()
     {
         $model = new Estimate;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getBills()
     {
         $model = new Bill;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getInvoices()
     {
         $model = new Invoice;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getItems()
     {
         $model = new Item;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getRecurringBills()
     {
         $model = new RecurringBill;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getRecurringInvoices()
     {
         $model = new RecurringInvoice;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getTasks()
     {
         $model = new Task;
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 
     public function getTimes()
     {
         $model = new Time();
-        $model->setRequestor($this->requestor);
+        $model->setRequestor($this->getRequestor());
         return $model->all();
     }
 }
