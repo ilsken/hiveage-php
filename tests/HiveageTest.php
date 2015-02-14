97f1390a2dba1633526cb4eadc01991e7e8392a9
Lance Pioch <lance@bouncefirst.com>
2015-02-14 17:40:22 -0500

    Add tests for main Hiveage class.

---
+++ tests/HiveageTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class HiveageTest extends PHPUnit_Framework_TestCase
+{
+    public function testRetrieveModels()
+    {
+        $methods = ['getConnections', 'getEstimates', 'getBills', 'getInvoices', 'getItems', 'getRecurringBills', 'getRecurringInvoices', 'getTasks', 'getTimes'];
+        $requestor = $this->getMock('Bouncefirst\Hiveage\Api\Requestor', [], ['test']);
+        $requestor->expects($this->exactly(count($methods)))->method('get');
+        $hiveage = new \Bouncefirst\Hiveage\Hiveage($requestor);
+
+        foreach ($methods as $method) {
+            $hiveage->$method();
+        }
+    }
+
+    public function testConstructor()
+    {
+        $hiveage = new \Bouncefirst\Hiveage\Hiveage('test', '12345');
+        $http = $hiveage->getRequestor()->getHttp();
+        $this->assertEquals('https://test.hiveage.com/api/', $http->getBaseUrl());
+        $this->assertEquals(['12345', ''], $http->getDefaultOption('auth'));
+    }
+}
